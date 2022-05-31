b298ec153d089f02efdcabdf2cf87215db4bd49f
Kayla <kaymckay@umich.edu>
2022-05-30 21:17:19 -0700

    update manifest, remove index.html

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../App.scss'
 import type { AppProps } from 'next/app'
+import Head  from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+   
+  return (
+   <>
+      <Head>
+         <title>Shroom Finder</title>
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+   </>
+  )
 }
